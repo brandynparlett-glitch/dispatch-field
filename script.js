2b6b49e8d3446bb5ce30cc122bb6bc67a697fcf2
brandynparlett-glitch <brandynparlett@gmail.com>
2025-11-22 18:03:30 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+const testButton = document.getElementById("testBtn");
+
+if (!testButton) {
+console.error("testBtn not found in the HTML.");
+return;
+}
+
+testButton.addEventListener("click", () => {
+alert("Your JavaScript is working!");
+});
+});
